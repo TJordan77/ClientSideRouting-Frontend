97f75d4f616e16e7166fd2648b7d6c19469313dd
TJordan77 <tvon777@gmail.com>
2025-07-01 00:22:30 -0400

    404 handling

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h2>404 – Page Not Found</h2>
+      <p>Preeeeeetty sure the page you're looking for doesn't exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
